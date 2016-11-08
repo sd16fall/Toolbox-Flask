90f5235414e3a8dbf47b43c5bd52dff5f88ab08f
arpanrau <arpan.rau@gmail.com>
2016-11-08 17:32:41 -0500

    Finished Toolbox, now with changes tracked

--- hello.py
+++ hello.py
@@ -1,13 +1,37 @@
 """
-Simple "Hello, World" application using Flask
+Site that requests name, age, and favorite softdes ninja and displays user profile
 """
-
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
+#Index Page
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+#Form results page
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    #if post request(form submit)
+    if request.method == 'POST':
+        #read form results, if present return profile page template
+        name = request.form['firstname']
+        age = request.form['age']
+        ninja = request.form['favoriteninja']
+        if name and age and ninja:
+            return render_template('profile.html' ,name = name, age = age, ninja = "Patrick Huston")
+        else:
+        #otherwise return error page template
+            return render_template('error.html')
+    #if get request return form for submit
+    else:
+        return render_template('index.html')
+    
 
 if __name__ == '__main__':
     app.run()
+    
